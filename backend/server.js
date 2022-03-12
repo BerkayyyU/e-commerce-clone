36e2630afed760198a14f5dadde21f005eb41773
Berkay Ulguel <berkayyyulguel@gmail.com>
2022-03-12 17:51:01 +0300

    database connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
@@ -9,12 +9,13 @@ dotenv.config(); // to read .env file and use
 
 const app = express();
 app.use(express.json()); // parsing json data in the body of request
-app.use(express.urlencoded( {extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/e-commerce", { // To connect with mongodb database. If MONGODB_URL exists us it otherwise use the other one.
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/e-commerce', {
+  // To connect with mongodb database. If MONGODB_URL exists use it otherwise use the other one.
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 // app.get('/api/products/:id', (req, res)=>{
@@ -26,25 +27,25 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/e-commerce", { 
 //     }
 // });
 
-app.use("/api/users", userRouter);
+app.use('/api/users', userRouter);
 
-app.use("/api/products", productRouter); // responder is productRouter
+app.use('/api/products', productRouter); // responder is productRouter
 
-app.use("/api/orders", orderRouter);
+app.use('/api/orders', orderRouter);
 
 // app.get('/api/products', (req, res)=>{ We remove this cause of we are no longer sending data from data.js statically. we are getting data from mongodb
 //     res.send(data.products);
 // });
 
 app.get('/', (req, res) => {
-    res.send("Server hazır");
+  res.send('Server hazır');
 });
 
 app.use((err, req, res, next) => {
-    res.status(500).send({message: err.message}); // error wil be directed to this function from userRouter expressasynchandler and the same error wil be send back to frontend
+  res.status(500).send({ message: err.message }); // error wil be directed to this function from userRouter expressasynchandler and the same error wil be send back to frontend
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () =>{
-    console.log(`Serve at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
 });
